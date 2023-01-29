46064b83626ab3898341b73e6cc19cecaa56f26e
yyusronwirawan <yusronwirawan@yahoo.com>
2023-01-29 18:49:53 +0700

    first commit

--- src/pages/Frontpage.jsx
+++ src/pages/Frontpage.jsx
@@ -49,7 +49,7 @@ function Frontpage() {
               <a href="#contact_page">
                 <button className="btn primary-btn">Hire Me</button>
               </a>
-              <a href="./yusronresume.pdf" download>
+              <a href="./yusron_resume.pdf" download>
                 <button className="btn highlighted-btn">Get Resume</button>
               </a>
             </div>
